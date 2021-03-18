536ff0461dd53e46a3861ecc2d9c008929de4129
Matt Serrato <mattserrato@Matts-MBP-2.fibertel.com.ar>
2021-03-18 15:09:28 -0300

    mode dev and prod for webpack wip on render files on react file-loader

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import Img from './lacoste-logo.png';
 
-const App = () => <div>React from scratch, testing webpack serve</div>
+const App = () => {
+  return (
+    <div>
+      React from scratch, testing webpack serve
+      <img src={Img} alt='lacoste logo'></img>
+    </div>
+  )
+}
 
 ReactDom.render(<App />, document.getElementById('root'))
